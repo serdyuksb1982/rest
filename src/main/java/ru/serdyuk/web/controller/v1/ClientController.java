a78369a0aa6828ce1b8d98040c94e25c75be0d74
79267 <serdyuksb@gmail.com>
2024-03-11 22:17:15 +0300

    OpenApi and Swagger - config from Open Api.
    Добавлен класс конфигурации SwaggerConfig и описание ряда методов в контроллере (пример)

--- src/main/java/ru/serdyuk/web/controller/v1/ClientController.java
+++ src/main/java/ru/serdyuk/web/controller/v1/ClientController.java
@@ -1,15 +1,21 @@
 package ru.serdyuk.web.controller.v1;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.serdyuk.exception.EntityNotFoundException;
 import ru.serdyuk.mapper.v1.ClientMapper;
 import ru.serdyuk.model.Client;
 import ru.serdyuk.service.ClientService;
 import ru.serdyuk.web.model.ClientListResponse;
 import ru.serdyuk.web.model.ClientResponse;
+import ru.serdyuk.web.model.ErrorResponse;
 import ru.serdyuk.web.model.UpsetClientRequest;
 
 import javax.validation.Valid;
@@ -17,12 +23,18 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("api/v1/client")
 @RequiredArgsConstructor
+@Tag(name = "Client V1", description = "Client API: version V1")
 public class ClientController {
 
     public final ClientService clientService;
 
     public final ClientMapper clientMapper;
 
+    @Operation(
+            summary = "Get all clients",
+            description = "Get all clients, and return list -> id, name and list of orders",
+            tags = {"client"}
+    )
     @GetMapping
     public ResponseEntity<ClientListResponse> findAll() {
         return ResponseEntity.ok(
@@ -30,6 +42,21 @@ public class ClientController {
         );
     }
 
+    @Operation(
+            summary = "Get client by ID",
+            description = "Get client by ID, and return id, name and list of orders",
+            tags = {"client", "id"}
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {@Content(schema = @Schema(implementation = ClientResponse.class), mediaType = "application/json")}
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")}
+            )}
+    )
     @GetMapping("/{id}")
     public ResponseEntity<ClientResponse> findById(@PathVariable Long id) {
         return ResponseEntity.ok(
